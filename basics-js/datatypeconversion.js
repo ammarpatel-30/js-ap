7a60af5c55b6a1370e4befc178a3f531fc93cd2d
ammarpatel-30 <ammupatel112@gmail.com>
2025-08-31 04:18:10 +0000

    container config done

---
+++ basics-js/datatypeconversion.js
@@ -0,0 +1,10 @@
+let num = 56
+console.log(typeof num)
+let valueInNumber = Number(num)
+// In some cases the number might be in string so we have to convert into number some times
+console.log(typeof valueInNumber)
+
+let isLoggedIn = 1
+console.log(typeof isLoggedIn)
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn)
