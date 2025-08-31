da89df6066b11713d02b6e087b0926ee54396ae7
ammarpatel-30 <ammupatel112@gmail.com>
2025-08-31 03:53:29 +0000

    container commit done

---
+++ basics-js/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // Treat all JavaScript code as new version
+
+let name = "Ammar"  // String
+let age = 19        // Number
+let city = undefined//undefined
+console.log([ name, age, city])
+
+// typeof is used to find which datatype is used.
+console.log(typeof "name")
+console.log(typeof age)
+console.log(typeof null)  //null's type is object
+console.log(typeof undefined)
+
+
+ 
